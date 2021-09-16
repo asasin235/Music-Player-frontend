c1ee8eafe49046a488bab27703b1614c4f3c5722
asasin235 <aatif18789@st.jmi.ac.in>
2021-09-16 21:24:47 +0530

    Edited Readme

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Music Player To be Made
+          Music Player To be Made"
         </p>
         <a
           className="App-link"
