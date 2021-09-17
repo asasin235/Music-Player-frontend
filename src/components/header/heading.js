16aa264b29fd178bcb149e3e588459e5eeb598b8
neeraj2403 <jayarajneeraj@gmail.com>
2021-09-17 18:01:13 +0530

    Header

---
+++ src/components/header/heading.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import  './header.css';
+
+const heading = ()=>{
+
+        return (
+            <div className = "heading">
+                <h1>Play My Playlist</h1>
+                <hr/>
+                <p>A multiplayer Social Game  to play along with your friends in a private room</p>
+            </div>
+        )
+    
+}
+
+export default heading
