cbe7ad269130f0a561dd8a491c667edea406578f
Nehal Ahmad <52347191+Nehal98@users.noreply.github.com>
2021-09-21 01:36:59 -0400

    btn

---
+++ src/components/createbtn/createbtn.js
@@ -0,0 +1,18 @@
+import React  from 'react'
+import { Button } from 'react-bootstrap';
+import  './createbtn.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Createbtn = ()=>{
+
+    return (
+        <div className="createbtn">
+          <Button href="#"><b>Create Room</b></Button>
+        </div>
+
+        
+    );
+
+}
+
+export default Createbtn
