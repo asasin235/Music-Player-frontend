afda575f06109819703b166ec58221ad983670ed
asasin235 <aatif18789@st.jmi.ac.in>
2021-09-23 11:48:24 +0530

    Added Coming Soon

---
+++ src/pages/CreateRoom/CreateRoom.js
@@ -0,0 +1,13 @@
+
+import Link from 'react-router-dom/Link'
+const CreateRoom =()=>
+{
+    return( 
+    <>
+    <h1>Men at Work</h1>
+    <Link to={'/'}> Go Back</Link>
+    </>
+    )
+}
+
+export default CreateRoom
--- src/pages/JoinRoom/JoinRoom.js
+++ src/pages/JoinRoom/JoinRoom.js
@@ -1,6 +1,11 @@
+import Link from "react-router-dom/Link"
 const JoinRoom=()=>{
     return (
         <>
+        <>
+    <h1>Men at Work</h1>
+    <Link to={'/'}> Go Back</Link>
+    </>
         </>
     )
 }
